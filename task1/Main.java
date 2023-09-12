352ddec1f9674e5834b6bec6680c7096c5cfb5ef
DmitriyPanfilov <126699728+DmitriyPanfilov@users.noreply.github.com>
2023-09-12 21:11:54 +0300

    Task 1

---
+++ task1/Main.java
@@ -0,0 +1,12 @@
+package lesson_4.homework4.task1;
+
+public class Main {
+    public static void main(String[] args) {
+        PrintElement element = new PrintElement();
+
+        element.printElement(32);
+        element.printElement("Hello");
+        element.printElement(3.15);
+        element.printElement(true);
+    }
+}
---
+++ task1/PrintElement.java
@@ -0,0 +1,7 @@
+package lesson_4.homework4.task1;
+
+public class PrintElement {
+    public <T> void printElement(T param){
+        System.out.println(param);
+    }
+}
